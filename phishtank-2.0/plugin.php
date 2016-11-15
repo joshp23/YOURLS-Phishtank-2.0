9dc37e4e8939184f5473b8f982c4a80b381ee30a
Josh Panter <joshu@unfettered.net>
2016-11-15 12:18:29 -0500

    Add option in preferences to disable recheck on redirect (#2)
    * Update plugin.php
    
    * Update README.md
    
    * Update README.md

--- phishtank-2.0/plugin.php
+++ phishtank-2.0/plugin.php
@@ -29,14 +29,20 @@ function phishtank_do_page() {
 		// Process form - update option in database
 		yourls_update_option( 'phishtank_api_key', $_POST['phishtank_api_key'] );
 		if(isset($_POST['phishtank_soft'])) yourls_update_option( 'phishtank_soft', $_POST['phishtank_soft'] );
+		if(isset($_POST['phishtank_rechech'])) yourls_update_option( 'phishtank_rechech', $_POST['phishtank_rechech'] );
 	}
 
-	// Get value from database
+	// Get values from database
 	$phishtank_api_key = yourls_get_option( 'phishtank_api_key' );
 	$phishtank_soft = yourls_get_option( 'phishtank_soft' );
 	
-	if ($phishtank_soft == "1") $ck = 'checked';
-	if ($phishtank_soft == "0") $ck = null;
+	if ($phishtank_soft == "true") $ck = 'checked';
+	if ($phishtank_soft == "false" || $phishtank_soft == null) $ck = null;
+
+	$phishtank_rechech = yourls_get_option( 'phishtank_rechech' );
+	
+	if ($phishtank_rechech == "true" || $phishtank_rechech == null) $rck = 'checked';
+	if ($phishtank_rechech == "false") $rck = null;
 
 	// Create nonce
 	$nonce = yourls_create_nonce( 'phishtank' );
@@ -48,13 +54,22 @@ function phishtank_do_page() {
 		<input type="hidden" name="nonce" value="$nonce" />
 		<p><label for="phishtank_api_key">Your Key  </label> <input type="text" size=60 id="phishtank_api_key" name="phishtank_api_key" value="$phishtank_api_key" /></p>
 
-		<h2>Preserve old links?</h2>
-		<p>Old links will be re-checked every time that they are clicked. You can decide how to handle old links that are found to be dirty on a re-check here. <b>Default is to delete them.</b> Check below to preserve these dirty old links, and display a warning page on redirect isntead.</p>
-		
+		<h2>Redirect Rechecks: old links behavior</h2>
+		<p>Old links can be re-checked every time that they are clicked. <b>Default behavior is to check them</b>.</p>
+
 		<div class="checkbox">
 		  <label>
-		    <input type="hidden" name="phishtank_soft" value="0" />
-		    <input name="phishtank_soft" type="checkbox" value="1" $ck > Soft on Spam?
+		    <input type="hidden" name="phishtank_rechech" value="false" />
+		    <input name="phishtank_rechech" type="checkbox" value="true" $rck > Recheck old links?
+		  </label>
+		</div>
+
+		<p>You can decide how to handle old links that are found to be dirty on a re-check here. <b>Default is to delete them.</b> Check below to preserve these dirty old links, and display a warning page on redirect isntead.</p>
+
+		<div class="checkbox">
+		  <label>
+		    <input type="hidden" name="phishtank_soft" value="false" />
+		    <input name="phishtank_soft" type="checkbox" value="true" $ck > Soft on Spam?
 		  </label>
 		</div>
 		<p><input type="submit" value="Submit" /></p>
@@ -100,33 +115,37 @@ function phishtank_check_add( $false, $url ) {
 // Re-Check phishtank on redirection
 yourls_add_action( 'redirect_shorturl', 'phishtank_check_redirect' );
 function phishtank_check_redirect( $url, $keyword = false ) {
-	
-	if( is_array( $url ) && $keyword == false ) {
-		$keyword = $url[1];
-		$url = $url[0];
-	}
-	
-	// Check when the link was added
-	// If shorturl is fresh (ie probably clicked more often?) check once every 15 times, otherwise once every 5 times
-	// Define fresh = 3 days = 259200 secondes
-	// TODO: when there's a shorturl_meta table, store last check date to allow checking every 2 or 3 days
-	$now  = date( 'U' );
-	$then = date( 'U', strtotime( yourls_get_keyword_timestamp( $keyword ) ) );
-	$chances = ( ( $now - $then ) > 259200 ? 15 : 5 );
-	
-	if( $chances == mt_rand( 1, $chances ) ) {
-		if( phishtank_is_blacklisted( $url ) != false ) {
-			$phishtank_soft = yourls_get_option( 'phishtank_soft' );
-			if( $phishtank_soft == "1" ) display_phlagpage();
-			if( $phishtank_soft == "0" ) {
-				// Delete link & die
-				yourls_delete_link_by_keyword( $keyword );
-				yourls_die( 'This domain has been blacklisted. This short URL has been deleted from our record.', 'Domain blacklisted', '403' );
-			} 
-		}
-	}
-	// Nothing, move along
 
+	$phishtank_rechech = yourls_get_option( 'phishtank_rechech' );
+	if ($phishtank_rechech == "true" || $phishtank_rechech == null) {
+
+		if( is_array( $url ) && $keyword == false ) {
+			$keyword = $url[1];
+			$url = $url[0];
+		}
+	
+		// Check when the link was added
+		// If shorturl is fresh (ie probably clicked more often?) check once every 15 times, otherwise once every 5 times
+		// Define fresh = 3 days = 259200 secondes
+		// TODO: when there's a shorturl_meta table, store last check date to allow checking every 2 or 3 days
+		$now  = date( 'U' );
+		$then = date( 'U', strtotime( yourls_get_keyword_timestamp( $keyword ) ) );
+		$chances = ( ( $now - $then ) > 259200 ? 15 : 5 );
+	
+		if( $chances == mt_rand( 1, $chances ) ) {
+			if( phishtank_is_blacklisted( $url ) != false ) {
+				$phishtank_soft = yourls_get_option( 'phishtank_soft' );
+				if( $phishtank_soft == "true" ) display_phlagpage();
+				if( $phishtank_soft == "false" || $phishtank_soft == null) {
+					// Delete link & die
+					yourls_delete_link_by_keyword( $keyword );
+					yourls_die( 'This domain has been blacklisted. This short URL has been deleted from our record.', 'Domain blacklisted', '403' );
+				} 
+			}
+		}
+		// Nothing found, move along
+	}
+	// Re-check disabled, move along
 }
 // Soft on Spam ~ interstitial warning
 function display_phlagpage($keyword) {
@@ -174,7 +193,7 @@ function phishtank_is_blacklisted( $url ) {
         $ch = curl_init();
         curl_setopt ($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt ($ch, CURLOPT_POST, TRUE);
-        curl_setopt ($ch, CURLOPT_USERAGENT, "x90");
+        curl_setopt ($ch, CURLOPT_USERAGENT, "YOURLS");
         curl_setopt ($ch, CURLOPT_POSTFIELDS, "format=xml&app_key=$phishtank_api_key&url=$url_64");
         curl_setopt ($ch, CURLOPT_URL, "$API");
         $result = curl_exec($ch);
